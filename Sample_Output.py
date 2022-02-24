efa56b054017b421328aa3b83c1bc4d814e11c02
Gunin Wasan <98042082+guninwasan@users.noreply.github.com>
2022-02-24 12:31:54 -0500

    Showing the sample output of the program

---
+++ Sample_Output.py
@@ -0,0 +1,165 @@
+"""
+This is what the sample output will look like upon running the program
+OUTPUT:
+------------------
+
+MENU OPTIONS:
+1.Create a new database
+2.Create Employee Details Table
+3.Create Employee Pay Slip Table
+4. Enter Employee details and salary information
+5. Display Tables
+6. Delete an Employee Record
+7. Search for an Employee Record
+0. Exit
+Enter Menu Option=1
+DATABASE CREATED
+------------------
+MENU OPTIONS:
+1.Create a new database
+2.Create Employee Details Table
+3.Create Employee Pay Slip Table
+4. Enter Employee details and salary information
+5. Display Tables
+6. Delete an Employee Record
+7. Search for an Employee Record
+0. Exit
+Enter Menu Option=2
+Table EMPLOYEE_DETAILS CREATED
+------------------
+MENU OPTIONS:
+1.Create a new database
+2.Create Employee Details Table
+3.Create Employee Pay Slip Table
+4. Enter Employee details and salary information
+5. Display Tables
+6. Delete an Employee Record
+7. Search for an Employee Record
+0. Exit
+Enter Menu Option=3
+Table EMPLOYEE_PAYSLIP CREATED
+------------------
+MENU OPTIONS:
+1.Create a new database
+2.Create Employee Details Table
+3.Create Employee Pay Slip Table
+4. Enter Employee details and salary information
+5. Display Tables
+6. Delete an Employee Record
+7. Search for an Employee Record
+0. Exit
+Enter Menu Option=4
+Enter the number of employees=3
+Enter Employee Name=MUNNAPPA
+YOUR AUTO-GENERATED EMPLOYEE NUMBER IS= 1001
+Employee Gender=M
+Enter the Birth Year=1950
+Enter the Birth Month=MARCH
+Input Birth Month Again
+Enter the Birth Month=3
+Enter the Day of Birth=31
+Enter the join Year=1999
+Enter the join Month=12
+Enter the day of joining=12
+Enter the designation of the Employee=DRIVER
+Enter the Basic Salary of the Employee=12000
+12000.0
+Enter the employee mobile number=98762801
+Enter the employee home number=22346819
+Enter employee address=ABU HALIFA
+Enter Employee Name=SUDARSHAN
+YOUR AUTO-GENERATED EMPLOYEE NUMBER IS= 1002
+Employee Gender=M
+Enter the Birth Year=1987
+Enter the Birth Month=7
+Enter the Day of Birth=7
+Enter the join Year=2010
+Enter the join Month=8
+Enter the day of joining=10
+Enter the designation of the Employee=MANAGER
+Enter the Basic Salary of the Employee=35000
+35000.0
+Enter the employee mobile number=98765201
+Enter the employee home number=22736389
+Enter employee address=MAHBOULA
+Enter Employee Name=KESHAV
+YOUR AUTO-GENERATED EMPLOYEE NUMBER IS= 1003
+Employee Gender=M
+Enter the Birth Year=1980
+Enter the Birth Month=11
+Enter the Day of Birth=12
+Enter the join Year=2109
+Enter the join Year=2019
+Enter the join Month=1
+Enter the day of joining=28
+Enter the designation of the Employee=SECURITY 
+Enter the Basic Salary of the Employee=20000
+20000.0
+Enter the employee mobile number=982018391
+Enter the employee mobile number=92893130
+Enter the employee home number=22347847
+Enter employee address=FAHAHEEL
+Name of the Month=JANUARY
+Which Year=2019
+Enter the number of days worked=25
+EMPLOYEE DATA HAS BEEN INPUTTED
+------------------
+MENU OPTIONS:
+1.Create a new database
+2.Create Employee Details Table
+3.Create Employee Pay Slip Table
+4. Enter Employee details and salary information
+5. Display Tables
+6. Delete an Employee Record
+7. Search for an Employee Record
+0. Exit
+Enter Menu Option=5
+MUNNAPPA	||  1001    ||  M   ||  1950-03-31  ||  1999-12-12  ||  DRIVER	||12000.00  ||  98762801    ||  22346819    ||  ABU HALIFA	||  
+SUDARSHAN	||  1002    ||  M   ||  1987-07-07  ||  2010-08-10  ||  MANAGER	||35000.00  ||  98765201    ||  22736389    ||  MAHBOULA	||  
+KESHAV	        ||  1003    ||  M   ||  1980-11-12  ||  2019-01-28  ||  SECURITY||20000.00  ||  92893130    ||  22347847    ||  FAHAHEEL	||  
+------------
+-----------
+----------
+DISPLAYING PAYSLIP FOR= 1003   MONTH=JANUARY   NO.OF DAYS WORKED=25
+
+KESHAV	    1003	    17857.14	    9821.43	    6250.00	    2678.57	    36607.14	    
+------------------
+MENU OPTIONS:
+1.Create a new database
+2.Create Employee Details Table
+3.Create Employee Pay Slip Table
+4. Enter Employee details and salary information
+5. Display Tables
+6. Delete an Employee Record
+7. Search for an Employee Record
+0. Exit
+Enter Menu Option=6
+Enter Employee code you want to delete=1003
+RECORD DELETED
+------------------
+MENU OPTIONS:
+1.Create a new database
+2.Create Employee Details Table
+3.Create Employee Pay Slip Table
+4. Enter Employee details and salary information
+5. Display Tables
+6. Delete an Employee Record
+7. Search for an Employee Record
+0. Exit
+Enter Menu Option=7
+Enter Employee code you want to search=1003
+Row with empcode=1003 was not found
+------------------
+MENU OPTIONS:
+1.Create a new database
+2.Create Employee Details Table
+3.Create Employee Pay Slip Table
+4. Enter Employee details and salary information
+5. Display Tables
+6. Delete an Employee Record
+7. Search for an Employee Record
+0. Exit
+Enter Menu Option=0
+>>>
+
+"""
